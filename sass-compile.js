b64cafae1b536d423b93cf875ec207a8fc1f9ab6
Bejor <bernhard.waitz@oegbverlag.at>
2025-01-17 13:50:46 +0100

    Test

--- sass-compile.js
+++ sass-compile.js
@@ -4,18 +4,4 @@ import { fileURLToPath } from 'url';
 import { compileDirectoriesInPlace, watchAndCompile } from './build/sass-util.js';
 
 console.log(import.meta.url);
-
-const IGNORED_FILES = ['_breakpoints.scss', '_typography.scss']; // list of files to ignore from compilation
-const SASS_FOLDERS = ['styles', 'blocks']; // list of folders to compile
-
-// MAIN
-
-const args = process.argv.slice(2);
-if (args.includes('--watch')) {
-  await watchAndCompile(path.join(dirname), IGNORED_FILES);
-} else if (args.includes('--compile')) {
-  const directories = SASS_FOLDERS.map((folder) => path.join(dirname, folder));
-  await compileDirectoriesInPlace(directories, IGNORED_FILES);
-} else {
-  console.log('Please specify a flag: --watch or --compile');
-}
+return false;
